602978081b88cacc4ce3ed5da617a988317ae309
ilgabrie <36480642+ilgabrie@users.noreply.github.com>
2021-09-16 17:19:55 +0200

    Create variables.tf

---
+++ iac/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "dataops-dev"
+}
+variable "location" {
+  description = "Azure location"
+  type        = string
+  default     = "westeurope"
+}
