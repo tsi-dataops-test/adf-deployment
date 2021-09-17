ee27460cae2540efb123fed532124028d67dcebf
ilgabrie <36480642+ilgabrie@users.noreply.github.com>
2021-09-17 11:10:25 +0200

    Create variables.tf

---
+++ iac/variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "dataopsprod"
+}
+variable "location" {
+  description = "Azure location"
+  type        = string
+  default     = "westeurope"
+}
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default     = {
+    group     = "dataops-test",
+    environment = "prod"
+  }
+}
